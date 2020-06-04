f9d9b201c601b19396c3babfa10764fd2970b462
AjayLiu <ajayliu2928@gmail.com>
2020-06-03 22:41:23 -0700

    moved content up directory

--- src/api.js
+++ src/api.js
@@ -6,9 +6,21 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
-    hello: "hi!"
+    "ok": "hi!"
   });
 });
+
+var geocoder = require('local-reverse-geocoder');
+var point = {latitude: 42.083333, longitude: 3.1};
+geocoder.lookUp(point, function(err, res) {
+    console.log(JSON.stringify(res, null, 2));
+});
+router.get("/geo", (req, res) => {
+    res.json({
+        "hello": "hi!"
+    });
+});
+
 
 app.use(`/.netlify/functions/api`, router);
 
